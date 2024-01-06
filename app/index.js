58265ab346ecfdaec9362c2b165d168d60181fdc
Santiago Rivera <santiagor10764@gmail.com>
2024-01-06 17:08:49 -0500

    Added assets, modified starter code and imported all neceessary files to index.js

--- app/index.js
+++ app/index.js
@@ -1,10 +1,25 @@
-import {View, Text} from 'react-native';
+import {useState} from 'react';
+import {View, ScrollView, SafeAreaView} from 'react-native';
+import { Stack, useRouter } from 'expo-router';
+
+import {COLORS, icons, images, SIZES} from '../constants';
+import {
+    NearbyJobs, PopularJobs, ScreenHeaderBtn, Welcome
+} from '../constants';
+
 
 const Home = () => {
+    const router = useRouter();
+
     return(
-        <View>
-            <Text>Home</Text>
-        </View>
+        <SafeAreaView style = {{flex: 1, backgroundColor: COLORS.lightWhite}}>
+            <Stack.Screen 
+                options={{
+                    headerStyle: {backgroundColor: COLORS.lightWhite},
+                    
+                }}
+            />
+        </SafeAreaView>
     )
 }
 
