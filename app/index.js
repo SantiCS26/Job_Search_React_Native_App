57a4a6489fbaaaf2dd6c78e70df1ee414aa6eb79
Santiago Rivera <santiagor10764@gmail.com>
2024-01-07 00:40:20 -0500

    Modifying the home screen to have a search bar, welcome message and options to navigate to

--- app/index.js
+++ app/index.js
@@ -4,8 +4,8 @@ import { Stack, useRouter } from 'expo-router';
 
 import {COLORS, icons, images, SIZES} from '../constants';
 import {
-    NearbyJobs, PopularJobs, ScreenHeaderBtn, Welcome
-} from '../constants';
+    Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome
+} from '../components';
 
 
 const Home = () => {
@@ -13,12 +13,38 @@ const Home = () => {
 
     return(
         <SafeAreaView style = {{flex: 1, backgroundColor: COLORS.lightWhite}}>
+
             <Stack.Screen 
                 options={{
                     headerStyle: {backgroundColor: COLORS.lightWhite},
-                    
+                    headerShadowVisible: false,
+                    headerLeft: () => (
+                        <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+                    ),
+                    headerRight: () => (
+                        <ScreenHeaderBtn iconUrl={images.profile} dimension="100%" />
+                    ),
+                    headerTitle: ""
                 }}
             />
+
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <View
+                    style={{
+                        flex: 1,
+                        padding: SIZES.medium
+                    }}
+                >
+                    <Welcome
+                    
+                    />
+
+                    <Popularjobs />
+                    <Nearbyjobs />
+
+                </View>
+            </ScrollView>
+
         </SafeAreaView>
     )
 }
--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,27 @@
 import {stack} from 'expo-router';
+import { useCallback } from 'react';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync();
+
 
 const Layout = () => {
-    return <stack />;
+    const [fontsLoaded] = useFonts({
+        DMBold: require('../assets/fonts/DMSans-Bold.ttf'),
+        DMMedium: require('../assets/fonts/DMSans-Medium.ttf'),
+        DMRegular: require('../assets/fonts/DMSans-Regular.ttf'),
+    })
+
+    const onLayoutRootView = useCallback(async() => {
+        if(fontsLoaded){
+            await SplashScreen.hideAsync();
+        }
+    }, [fontsLoaded])
+
+    if(!fontsLoaded) return null;
+
+    return <stack onLayout ={onLayoutRootView}/>;
 }
 
 export default Layout;
